b2a6cd925709b3d5b4ca1d8e66576a963c3296dd
Elvis020 <spaceyel3@gmail.com>
2022-08-09 10:50:22 +0000

    Done with coutChars and added test🥷🏿

--- Scala_Exercises/src/main/scala/Week1/Recursion_Exercises/LittleMoreFun/LittleMoreFun.scala
+++ Scala_Exercises/src/main/scala/Week1/Recursion_Exercises/LittleMoreFun/LittleMoreFun.scala
@@ -1,8 +1,36 @@
 package Week1.Recursion_Exercises.LittleMoreFun
 
+import scala.collection.mutable
+
 class LittleMoreFun {
-  def countCharacters(s: String): Map[Char, Int] = {
-    ???
+  // Without recursion
+  def countCharacters(s: String):mutable.Map[Char,Int] = {
+    val emptyMap = scala.collection.mutable.Map[Char,Int]()
+    s.map(c =>{
+      emptyMap += ((c ,s.count(_ == c)))
+    })
+    emptyMap
+  }
+  def hasValidParenthesis(s:String): Boolean = ???
+
+
+  // With recursion
+  def countCharactersRT(s:String):mutable.Map[Char,Int] = {
+    def helperFunc(a:String,myMap:mutable.Map[Char,Int]=mutable.Map[Char,Int]()):mutable.Map[Char,Int]={
+      if (a=="") myMap
+      else {
+        myMap.updateWith(a.head){
+          case Some(count) => Some(count+1)
+          case None => Some(1)
+        }
+        helperFunc(a.tail,myMap)
+      }
+    }
+    helperFunc(s)
   }
 
+}
+
+object Test extends App{
+  val lf = new LittleMoreFun()
 }
---
+++ Scala_Exercises/src/test/scala/LittleMoreFunSuite.scala
@@ -0,0 +1,22 @@
+import Week1.Recursion_Exercises.LittleMoreFun.LittleMoreFun
+import munit.FunSuite
+
+import scala.collection.mutable
+
+class LittleMoreFunSuite extends FunSuite {
+  val littleMoreFun = new LittleMoreFun()
+
+//  test("Should the return false the given parenthesis ") {
+//    val obtained = littleMoreFun.hasValidParenthesis("")
+//    val expected = false
+//    assertEquals(obtained, expected)
+//  }
+
+
+  test("Should count the characters in a string using tail recursion") {
+    val obtained = littleMoreFun.countCharactersRT("hello")
+    val expected = mutable.HashMap('e' -> 1, 'h' -> 1, 'l' -> 2, 'o' -> 1)
+    assertEquals(obtained, expected)
+  }
+
+}
